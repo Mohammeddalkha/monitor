1afde4ad7bc99abb784b8810b9e6871fc93e175d
Mohamed dalkha <mohameddalkha43@gmail.com>
2025-03-15 15:38:49 +0530

    Hardcoded port in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -43,5 +43,5 @@ def get_status():
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
